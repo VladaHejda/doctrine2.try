6bdc5e6761d93555d040f35ff9b6c8a0ae915e20
VladaHejda <hejda@vspdata.cz>
2014-05-14 12:55:18 +0200

    test data

---
+++ test-data/products.sql
@@ -0,0 +1,8 @@
+INSERT INTO `products` (`id`, `name`, `price`, `description`) VALUES
+(3,	'Konzerva',	80,	'Papani pro pejsky'),
+(4,	'Parecek',	20,	'Tesco párky'),
+(5,	'Tatarák',	180,	'Eňo ňuňo tataráček.'),
+(6,	'Paštika',	5,	'Májka. To neni.'),
+(7,	'Humr',	580,	'Kéž by tu byl s náma'),
+(8,	'Klobása',	55,	'nejlepčí!'),
+(9,	'Šlehačka',	5,	'na rakvičku');
---
+++ test-data/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` (`id`, `name`) VALUES
+(1,	'Vladik'),
+(2,	'Pepik'),
+(3,	'Fanda Dolejš'),
+(4,	'Jenda Dolanský'),
+(5,	'Kukurukuku');
